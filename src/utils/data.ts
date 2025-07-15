21cc96173dda2a063778a146eb5e22b65ada924c
Danielle Slva de Melo <danielleesilva.4@gmail.com>
2025-07-14 23:24:56 -0400

    solving images

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -7,7 +7,7 @@ export const newsData: NewsItem[] = [
         excerpt: "A Semana Nacional do Meio Ambiente foi comemorada entre os dias 2 e 8 de junho no Instituto Federal de Educação, Ciência e Tecnologia de Rondônia (IFRO) Campus Vilhena.",
         date: "2024-06-08",
         category: "Meio Ambiente",
-        image: "/images/new1.jpg"
+        image: "assets/images/new1.jpg"
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ export const newsData: NewsItem[] = [
         excerpt: "Foram publicados os editais para o processo seletivo 2024 dos cursos técnicos integrados ao ensino médio.",
         date: "2024-06-05",
         category: "Educação",
-        image: "/images/new2.jpg"
+        image: "assets/images/new2.jpg"
     },
     {
         id: 3,
@@ -23,6 +23,6 @@ export const newsData: NewsItem[] = [
         excerpt: "Estudantes apresentam projetos inovadores na Feira de Ciências do Campus Vilhena, demonstrando criatividade e conhecimento técnico.",
         date: "2024-06-03",
         category: "Ciência",
-        image: "/images/new3.jpg"
+        image: "assets/images/new3.jpg"
     }
 ];
